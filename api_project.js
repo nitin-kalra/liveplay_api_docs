a33ddbf2bac3e65c5e16249e16bedde3c36d974c
nitin kalra <nitin.kalra@kiwitech.com>
2014-11-10 13:10:41 +0530

    Add Api document

---
+++ api_project.js
@@ -0,0 +1,10 @@
+define({
+  "name": "LivePlay API",
+  "version": "1.0.0",
+  "description": "API for the LivePlay application",
+  "apidoc": "<h1>General</h1>\n\n<h2>Authorization</h2>\n\n<p>Basic authorization using the pre-shared username and password is required for all HTTP based API calls in order to authenticate the user agent and ensure that an authorized application is making the requests.</p>\n\n<p>In addition, a MD5 hash using a pre-shared salt is used in order to authenticate the applications over socket connections.</p>",
+  "generator": {
+    "version": "0.2.6",
+    "time": "2014-11-07T07:11:06.979Z"
+  }
+});
